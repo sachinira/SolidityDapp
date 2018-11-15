0a3d8abafce65099d66e70110a3538158c1ffac5
sachini <sachipcranasinghe@gmail.com>
2018-11-15 11:41:47 +0530

    completed tutorial

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,12 +2,17 @@ pragma solidity ^0.4.24;
 
 contract Election {
 
+	//the first letter of contract name is capitalized
 	//2.model a candidate
 	struct Candidate {
 		uint id;
 		string name;
 		uint voteCount;
 	}
+
+
+	//store accounts that has voted
+	mapping(address => bool) public voters;
 
 	//1.store candidates we need to save the candidates 
 	mapping(uint => Candidate) public candidates; 
@@ -17,6 +22,11 @@ contract Election {
 	//if look for a candidate not stored a default value is returned 
 	//when we call this function it expects a key as an argument
 
+
+	//adding an event
+	event votedEvent (
+		  uint indexed _candidateId
+	);
 
 
 	//after returning the promise the items inside the result must be accessed using array repreasentation
@@ -33,10 +43,10 @@ contract Election {
 	//constructor will run when the app is migrated to the blockchain
 	//we are defining a constructor in solidity by defining a function as the same name as the contract
 	function Election() public {
-		//this isgoing to run whenever we deploy it to the blockchain
+		//this is going to run whenever we deploy it to the blockchain
 		//so it is declared public
 		//store value of candidates
-		//candidate = "Candidate 1"; //variable without an underscore is a state variable and itaccessible inside our contract belongs to our smart contract 
+		//candidate = "Candidate 1"; //variable without an underscore is a state variable and it accessible inside our contract belongs to our smart contract 
 		//inorder to interact with it we have to create a migration 
 		addCandidate("candidate 1");
 		addCandidate("candidate 2");
@@ -55,4 +65,30 @@ contract Election {
 
 	//voter can be anyone connected to the blockchain these are represented by addresses connected to the blockchain
 	//we get the access to these user accounts using the web3 library
+
+
+	//cast the vote
+	//for these functions we can pass metadata in solidity language
+	function vote (uint _candidateId) public {
+
+		//record if that voter has voted as one voter can 
+		//app.vote(1,{ from: web3.eth.accounts[0] }) is the command passed
+		//here from is a metadata so it is captcured by the msg.seder and saved it in the voters mapping
+		//we dont want this function to be called by anyone
+		//if address has not voted before
+		//require accept a condition which evaluate to ture condition execures
+		require(!voters[msg.sender]); //that this address is not in this mapping
+		require(_candidateId > 0 && _candidateId <=candidateCount);//require a valid candidate
+
+		//voting for a valud candidate
+		voters[msg.sender] = true;
+		//update candidate voteCount
+		//reference mapping
+		candidates[_candidateId].voteCount ++;
+
+
+		//we can create an event when the vote are counted ad subscribe to that event in our 
+		//front end application to see the votes coming in realtime
+		votedEvent(_candidateId);
+	}
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
-App = {
+ App = {
   web3Provider: null,
   contracts: {},
+  account: '0x0',
 
   init: function() {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
+   /* $.getJSON('../pets.json', function(data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
 
@@ -18,7 +19,7 @@ App = {
 
         petsRow.append(petTemplate.html());
       }
-    });
+    });*/
 
     return App.initWeb3();
   },
@@ -28,35 +29,172 @@ App = {
      * Replace me...
      */
 
+    /* // Modern dapp browsers...
+    if (App.ethereum) {
+        App.web3 = new Web3(ethereum);
+        try {
+            // Request account access if needed
+            ethereum.enable();
+            // Acccounts now exposed
+        } catch (error) {
+            // User denied account access...
+        }
+    }
+    // Legacy dapp browsers...
+    else if (App.web3) {
+        App.web3 = new Web3(web3.currentProvider);
+        // Acccounts always exposed
+    }
+    // Non-dapp browsers...
+    else {
+
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+
+    }*/
+
+
+
+
+     /*initilizes connection of clientside application to the blockchain*/
+
+     if(typeof web3 !== 'undefined'){
+      //If a web3 instance is already prvided by metamask
+      App.web3Provider = web3.currentProvider;
+      web3 =  new Web3(web3.currentProvider);
+     } else {
+      //specify a default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+     }
     return App.initContract();
   },
 
   initContract: function() {
     /*
-     * Replace me...
+     *  initilize the contract Election.js loads the contract into the front end application
      */
 
-    return App.bindEvents();
+     //this election.json works with election.sol file we use browsersync 
+     //it allows to read json files out of the build contract artifacts
+     $.getJSON("Election.json", function(election){ //load a jso file of the election artifact
+      //use this json to create a truffle contract
+      //truffle contract is the contractwhich can interact inside our app
+      App.contracts.Election = TruffleContract(election);
+      //set the provider in the contract 
+      App.contracts.Election.setProvider(App.web3Provider);
+
+      App.listenForEvents();
+
+       return App.render();
+
+     });
+
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  render:function(){
+    var electionInstance;
+
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    content.hide();
+
+
+    //display the account we are connecting to the blockchain with
+    web3.eth.getCoinbase(function(err, account){
+      if(err === null){
+        //we get the account instance
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
+
+
+    //list down the candidates on the page
+    App.contracts.Election.deployed().then(function(instance){
+      electionInstance = instance; 
+      //get instance of the artifact 
+      return electionInstance.candidateCount();
+      //the countercache of the candidates
+    }).then(function(candidateCount){
+
+      var candidatesResult = $("#candidatesResult");
+      candidatesResult.empty();
+
+      var candidatesSelect = $("#candidatesSelect");
+      candidatesSelect.empty();
+
+
+      //listout each candidate of the mapping we use our counter cache for this 
+      for(var i = 1;i <= candidateCount; i++){
+        electionInstance.candidates(i).then(function(candidate){
+
+          //fetch each cadidate from the mapping
+          var id = candidate[0];
+          var name = candidate[1];
+          var voteCount = candidate[2];
+
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
+          candidatesResult.append(candidateTemplate);
+          //we take the content and append it to the table on the page
+
+          var candidateOption = "<option value='" + id + "' >" + name + "</option>";
+          candidatesSelect.append(candidateOption);
+        });
+      }
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted){
+
+      if(hasVoted){
+
+        $('form').hide();
+
+      }
+
+      loader.hide();
+      content.show();
+      
+    }).catch(function(error){
+
+      console.warn(error);
+
+    });  
   },
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+  listenForEvents:function(){
+
+    App.contracts.Election.deployed().then(function(instance){
+
+      instance.votedEvent({},{  //the first argument is a filter /second is metadata this means we want to subscribe to every events on  
+        //in the blockchsin
+
+        fromBlock: 0,
+        toBlock: 'latest'
+
+      }).watch(function(error, event){
+
+        console.log("event trigered", event);
+        //to reload when a new vote is recorded
+        App.render();
+      });
+    });
   },
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+  castVote:function(){
 
-    var petId = parseInt($(event.target).data('id'));
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance){
 
-    /*
-     * Replace me...
-     */
+      return instance.vote(candidateId, {from: App.account});
+
+    }).then(function(result){
+      $('#content').hide();
+      $('#loader').show();
+    }).catch(function(error){
+      console.error(error);
+    });
+
   }
 
 };
